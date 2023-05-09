53c5b1b53d056335162272e019630909884e31e2
Abdurahman <abdurahmanborov@mail.ru>
2023-05-10 00:32:51 +0300

    added mockapi

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   React.useEffect(() => {
     async function PizzaBack() {
       const pizzaRes = await axios.get(
-        'https://file.notion.so/f/s/36ad4e93-800e-451b-9831-ae6abe1b28ef/pizzas.json?id=e934efcc-4042-481d-9d73-76f227f1696e&table=block&spaceId=b3238354-86d5-4ba6-9ad7-eb01112a9acd&expirationTimestamp=1683699692307&signature=t4X9XZ0whHWsOPcsJNg_F0Z374j2rjwaRnch5Hpsr18&downloadName=pizzas.json',
+        'https://645ab72565bd868e9322de44.mockapi.io/items',
       );
 
       setPizzaItem(pizzaRes.data);
